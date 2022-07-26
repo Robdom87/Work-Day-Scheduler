8c46955c84b900972ab551566785eda99c315da3
Cedar Chavez <geggas00@gmail.com>
2022-07-25 18:02:05 -0700

    add logic conceptual framework

---
+++ Develop/script.js
@@ -0,0 +1,9 @@
+//use moment js or an alt to post the date on the page and check what the current hour is\
+
+//change the formatting of the text area depending on the time of day
+
+//logic for save button to store in local storage and show the saved in local storage text and keep the text as the message
+
+//when time passes a past event is marked as passed
+
+//clear out at the end of the day?
