64b0b3fc39bde97d46cf61d937f84d19bf530b7a
Cedar Chavez <geggas00@gmail.com>
2022-07-26 22:37:58 -0700

    add function to check time of day and adjust formatting of time slots accordingly using jquery

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,4 @@
-//use moment js or an alt to post the date on the page and check what the current hour is\
+//use moment js or an alt to post the date on the page and check what the current hour is
 var weekday = "";
 var day = 0;
 var month = "";
@@ -37,18 +37,28 @@ setInterval(function () {
     month = monthName[dayjs().month()]; //0 to 11
     hour = dayjs().hour(); // 0 to 23 
 
+    //prints out current date
     currentDayEl.text(weekday+", "+month+" "+day);
 
+    //adjust the styling of each time block depending on the time of day
+    $('.hour').each(function(){
+        if ($( this ).data("time")===hour){
+            $( this ).parent().next().children().removeClass('future').addClass('present');
+
+        } else if ($( this ).data("time") < hour) {
+            $( this ).parent().next().children().removeClass('present').addClass('past');
+
+        } else {
+            $( this ).parent().next().children().removeClass('past').addClass('future');
+        }
+        return;
+    });
 
 }, 1000);
 
 
-//change the formatting of the text area depending on the time of day
-
 //logic for save button to store in local storage and show the saved in local storage text and keep the text as the message
 
-//when time passes a past event is marked as passed
+//when time passes a past event is marked as past
 
 //clear out at the end of the day?
-
-//dynamically generate the time slots?
